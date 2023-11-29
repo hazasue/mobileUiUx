3daf60af3b3d5979bbafaac785d1a0f78b692c4e
ChangHyeon Kim <hazasue@gmail.com>
2023-11-30 04:38:42 +0900

    :hammer: Add travel infos dynamically

--- Assets/Scripts/Controller/PanelManager.cs
+++ Assets/Scripts/Controller/PanelManager.cs
@@ -9,9 +9,14 @@ public class PanelManager : MonoBehaviour
     private const string PANEL_NAME_TRAVEL_INFO = "travelInfo";
 
     private Stack<GameObject> panels;
-    
+
+    public GameObject mainPanel;
     public GameObject travelPanel;
     public GameObject travelInfoPanel;
+
+    public Transform travelScreen;
+    
+    public GameObject infoBlock;
 
     // Start is called before the first frame update
     void Awake()
@@ -22,6 +27,7 @@ public class PanelManager : MonoBehaviour
     private void init()
     {
         panels = new Stack<GameObject>();
+        panels.Push(mainPanel);
     }
 
     public void PushPanelToStack(string name)
@@ -31,6 +37,8 @@ public class PanelManager : MonoBehaviour
         {
             case PANEL_NAME_TRAVEL:
                 tempObject = travelPanel;
+                updateInfos<Travel>(travelScreen,
+                    JsonManager.LoadJsonFile<Dictionary<string, Travel>>(JsonManager.JSON_FILENAME_TRAVEL));
                 break;
             
             case PANEL_NAME_TRAVEL_INFO:
@@ -42,13 +50,38 @@ public class PanelManager : MonoBehaviour
                 return;
         }
 
+        panels.Peek().SetActive(false);
+        
         panels.Push(tempObject);
         tempObject.SetActive(true);
     }
 
     public void PopPanelFromStack()
     {
-        GameObject tempObject = panels.Pop();
-        tempObject.SetActive(false);
+        panels.Pop().SetActive(false);
+        panels.Peek().SetActive(true);
+    }
+
+    private void updateInfos<T>(Transform panel, Dictionary<string, T> datas)
+    {
+        clearPanelInfos(panel);
+        instantiatePanelInfos<T>(panel, datas);
+    }
+
+    private void clearPanelInfos(Transform panel)
+    {
+        for (int i = panel.childCount - 1; i >= 0; i--)
+        {
+            Destroy(panel.GetChild(i).gameObject);
+        }
+    }
+
+    private void instantiatePanelInfos<T>(Transform panel, Dictionary<string, T> datas)
+    {
+        foreach (KeyValuePair<string, T> data in datas)
+        {
+            Instantiate(infoBlock, panel, true);
+        }
+
     }
 }
